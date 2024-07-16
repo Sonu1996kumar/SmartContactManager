80c6c87a107d964b599e320cf7722ee27ba47ac1
SONU KUMAR SINGH <sonukrr526@gmail.com>
2024-07-16 23:21:30 +0530

    Creating Responsive NavBar

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -2,6 +2,7 @@ package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +25,20 @@ public class PageController {
     @RequestMapping("/services")
     public String servicesPage(){
         return "services";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
     }
 }
