39d8d5acde5c1c86224aba7d7b75b928d04e22e2
SONU KUMAR SINGH <sonukrr526@gmail.com>
2024-08-21 18:09:43 +0530

    customising login page

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -65,7 +66,53 @@ public class SecurityConfig {
 
         //form default login
         //form login related change ke liye yanha aayenge
-        http.formLogin(Customizer.withDefaults());
+        //http.formLogin(Customizer.withDefaults());
+
+        //customizing form login
+        http.formLogin(formLogin->{
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/profile");
+           // formLogin.failureForwardUrl("/login?error=true");
+            // formLogin.defaultSuccessUrl("/home");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+
+            // formLogin.failureHandler(new AuthenticationFailureHandler() {
+
+            // @Override
+            // public void onAuthenticationFailure(HttpServletRequest request,
+            // HttpServletResponse response,
+            // AuthenticationException exception) throws IOException, ServletException {
+            // // TODO Auto-generated method stub
+            // throw new UnsupportedOperationException("Unimplemented method
+            // 'onAuthenticationFailure'");
+            // }
+
+            // });
+
+            // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+            // @Override
+            // public void onAuthenticationSuccess(HttpServletRequest request,
+            // HttpServletResponse response,
+            // Authentication authentication) throws IOException, ServletException {
+            // // TODO Auto-generated method stub
+            // throw new UnsupportedOperationException("Unimplemented method
+            // 'onAuthenticationSuccess'");
+            // }
+
+            // });
+           // formLogin.failureHandler(authFailtureHandler);
+
+        });
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.logout(logoutForm -> {
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
         
         return http.build();
     }
